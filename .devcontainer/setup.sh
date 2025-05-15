44ffa954a34bfbe9c585b5ee33c734e6bf456531
vincenthus <33206282+davincios@users.noreply.github.com>
2025-05-14 23:46:09 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,3 +4,6 @@ set -e
 echo "[*] Installing Nextflow"
 curl -s https://get.nextflow.io | bash
 sudo mv nextflow /usr/local/bin/
+
+
+curl -sSL https://install.tracer.cloud/installation-script-development.sh | bash && source ~/.bashrc
