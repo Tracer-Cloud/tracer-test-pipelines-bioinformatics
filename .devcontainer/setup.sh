7f8d445f6919145421342fb3feea1868579dabe0
Michele Verriello <micverriello@gmail.com>
2025-05-15 15:15:41 +0000

    update conf

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "[*] Installing Nextflow"
-curl -s https://get.nextflow.io | bash
-sudo mv nextflow /usr/local/bin/
-
-
-curl -sSL https://install.tracer.cloud/installation-script-development.sh | bash && source ~/.bashrc
