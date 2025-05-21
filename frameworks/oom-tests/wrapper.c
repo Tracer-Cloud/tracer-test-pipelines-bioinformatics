a85bf3609b6787a4554dc52bf05d08d954780ec6
David Onuh <davidonuh1@gmail.com>
2025-05-21 13:02:13 +0100

    rename python test lib

--- frameworks/oom-tests/wrapper.c
+++ frameworks/oom-tests/wrapper.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[]) {
     const char *python_bin = "/opt/conda/bin/python";
-    const char *script_path = "/root/tracer-test-pipelines-bioinformatics/frameworks/oom-tests/out_of_memory.py";
+    const char *script_path = "/root/tracer-test-pipelines-bioinformatics/frameworks/oom-tests/usecase_out_of_memory.py";
 
     char *const args[] = { "python", (char *)script_path, NULL };
     execv(python_bin, args);
