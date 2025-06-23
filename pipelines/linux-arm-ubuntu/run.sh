a8c43840162bc56fc0d3b3286a6e1f74dea740f1
arnetracer <arne@tracer.cloud>
2025-06-23 12:23:54 +0100

    Update run.sh

--- pipelines/linux-arm-ubuntu/run.sh
+++ pipelines/linux-arm-ubuntu/run.sh
@@ -18,6 +18,12 @@ else
     echo "[INFO] Nextflow already installed."
 fi
 
-# Step 2: Run the pipeline
+# Step 2: Prompt for user to run `tracer init`
+echo -e "\n🔧 Now run: tracer init"
+echo "🛑 The pipeline will stop here so you can configure Tracer."
+
+read -p "👉 Press Enter AFTER you've run 'tracer init' to continue..."
+
+# Step 3: Run the pipeline (user is assumed to have edited config properly)
 echo "[INFO] Running Nextflow pipeline..."
 nextflow run main.nf -c nextflow.config
