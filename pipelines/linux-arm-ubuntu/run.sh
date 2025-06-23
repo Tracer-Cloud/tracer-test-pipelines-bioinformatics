35448634f9f11d8f2173d87b190bef5d1e8cb5c7
arnetracer <arne@tracer.cloud>
2025-06-23 12:18:35 +0100

    Update run.sh

--- pipelines/linux-arm-ubuntu/run.sh
+++ pipelines/linux-arm-ubuntu/run.sh
@@ -4,16 +4,15 @@
 if ! command -v java &> /dev/null; then
     echo "[INFO] Java not found. Installing OpenJDK 17..."
     apt-get update -y
-    apt-get install -y openjdk-17-jre-headless
+    apt-get install -y openjdk-17-jre
 else
-    echo "[INFO] Java is already installed."
+    echo "[INFO] Java already installed."
 fi
 
 # Step 1: Ensure Nextflow is installed
 if ! command -v nextflow &> /dev/null; then
-    echo "[INFO] Nextflow not found. Installing..."
+    echo "[INFO] Nextflow not found, installing..."
     curl -s https://get.nextflow.io | bash
-    chmod +x nextflow
     mv nextflow /usr/local/bin/
 else
     echo "[INFO] Nextflow already installed."
